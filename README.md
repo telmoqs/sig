a0d1a56ce986f2cfa21a5f1e3dcf69428c93697d
Telmo Q Silva <telmoqs@gmail.com>
2021-07-09 16:05:57 -0300

    removido arquivos da branch main

--- README.md
+++
@@ -1,3 +0,0 @@
-# sig
-Java WEB; JDK 1.8; Spring 5; Hibernate 5.2; MySQL
-*de forma proposital, está faltando a conclusão da tela de "Cliente".
--- src/main/java/br/com/sig/enumeradores/Enumerador.java
+++
@@ -1,8 +0,0 @@
-package br.com.sig.enumeradores;
-
-public interface Enumerador {
-
-	public String getAbreviado();
-
-	public String getDescricao();
-}
